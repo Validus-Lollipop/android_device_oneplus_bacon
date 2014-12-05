680526bb634eefb70d40aaa23512c576bd65e7fa
martin coulon <martinusbe@gmail.com>
2014-12-05 19:55:52 +0100

    typo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,7 @@
 TARGET_KERNEL_CONFIG := cyanogenmod_bacon_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.selinux=permissive
 
-# Validus optimozations
+# Validus optimizations
 VALIDUS4LIFE=true
 
 # Enable ArchiDroid Optimizations
